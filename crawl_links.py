8fb0860430212865a6575c6143c7453bad3140dc
mamad <davoodabadiofficial@gmail.com>
2023-02-24 13:33:01 +0330

    link extracted

--- crawl_links.py
+++ crawl_links.py
@@ -5,11 +5,15 @@ farsroid_sitemap_url = "https://www.farsroid.com/sitemap.xml"
 
 r = requests.get(farsroid_sitemap_url, headers={"Accept": "application/xml"})
 
-print(r.status_code)
-print(r.headers['Content-type'])
-print(r.content.decode()[:100])
+
 results = bs(r.content, 'lxml-xml')
 results = results.find_all('sitemap')
 
-for i in results[-10:]:
-	print(i)
+for i in results:
+    link = i.find('loc').contents[0]
+
+    if "sitemap-pt" in link:
+        print(link)
+
+
+    
