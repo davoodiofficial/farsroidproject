99181195e6b54f6872fe488fbe085ce159d923b5
ali afraz <a.afraz.1380@gmail.com>
2023-02-24 12:54:27 +0330

    add xml parser

--- crawl_links.py
+++ crawl_links.py
@@ -1,1 +1,15 @@
-print("hello world")
+import requests
+from bs4 import BeautifulSoup as bs
+
+farsroid_sitemap_url = "https://www.farsroid.com/sitemap.xml"
+
+r = requests.get(farsroid_sitemap_url, headers={"Accept": "application/xml"})
+
+print(r.status_code)
+print(r.headers['Content-type'])
+print(r.content.decode()[:100])
+results = bs(r.content, 'lxml-xml')
+results = results.find_all('sitemap')
+
+for i in results[-10:]:
+	print(i)
