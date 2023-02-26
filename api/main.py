69419be7193bf88b981b3af93045451e876337f2
ali afraz <a.afraz.1380@gmail.com>
2023-02-27 01:31:36 +0330

    api is done. .gitignore updated.

--- api/main.py
+++ api/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from typing import Union
 import sqlite3
 
-SELECT_QUERY = """SELECT * FROM apps WHERE en_name like '%'||?||'%' or fa_name like '%'||?||'%'"""
+SELECT_QUERY1 = """SELECT * FROM apps WHERE en_name like '%'||?||'%' or fa_name like '%'||?||'%'"""
+SELECT_QUERY2 = """SELECT * FROM apps WHERE googleplay_link = ?"""
 
 app = FastAPI()
 
@@ -15,14 +16,20 @@ def home():
 @app.get('/name/')
 def get_app_by_name(fa_name: Union[str, None] = None, en_name: Union[str, None] = None):
     conn = sqlite3.connect('../crawler/apps.db')
+    conn.row_factory = sqlite3.Row
     cur = conn.cursor()
-    cur.execute(SELECT_QUERY, (en_name, fa_name))
+    cur.execute(SELECT_QUERY1, (en_name, fa_name))
     app = cur.fetchall()
-    cur.execute("select en_name from apps where id = 1")
-    print(f"'{cur.fetchall()}'")
+    conn.close()
     return {'app': app}
 
 
 @app.get('/url/')
 def get_app_by_url(url: Union[str, None] = None):
-    return {'app': url}
+    conn = sqlite3.connect('../crawler/apps.db')
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute(SELECT_QUERY2, (url, ))
+    app = cur.fetchall()
+    conn.close()
+    return {'app': app}
