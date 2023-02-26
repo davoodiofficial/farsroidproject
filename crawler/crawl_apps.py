1ed1b135aff658dad2aaeb147d342db476d94d9c
ali afraz <a.afraz.1380@gmail.com>
2023-02-27 02:38:19 +0330

    database feeded. crawler improved.

--- crawler/crawl_apps.py
+++ crawler/crawl_apps.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import sqlite3
+import sys
 
 DB_NAME = "apps.db"
+API = "https://www.farsroid.com/api/posts/?ids="
 CREATE_APP_TABLE_QUERY = """CREATE TABLE IF NOT EXISTS apps (
                                     id integer PRIMARY KEY,
                                     fa_name text NOT NULL,
@@ -16,57 +18,65 @@ INSERT_APP_QUERY = """INSERT INTO apps(fa_name,en_name,last_mod,download_box,goo
               VALUES(?,?,?,?,?,?)"""
 
 conn = sqlite3.connect(DB_NAME)
-
 cur = conn.cursor()
 cur.execute(CREATE_APP_TABLE_QUERY)
 
-URL = "https://www.farsroid.com/chrome-canary-android/"
-API = "https://www.farsroid.com/api/posts/?ids="
-LAST_MOD = '2023-02-25 16:11'
 
-page = requests.get(URL)
-# print(page.status_code)
+def crawl_app(link, last_mod):
+    global cur
+    global conn
 
-soup = BeautifulSoup(page.content, "html.parser")
-downloadbox = soup.find("table", class_="post-metas-tabeld")
-infos = downloadbox.find("tbody")
+    try:
+        page = requests.get(link)
+        soup = BeautifulSoup(page.content, "html.parser")
+        downloadbox = soup.find("table", class_="post-metas-tabeld")
+        infos = downloadbox.find("tbody")
+
+        googleplay_link = soup.find("a", class_="shadowed-btn gply-link")
+        title = googleplay_link.get("title")
+
+        download_box_info = ''
+
+        tr = soup.find_all("tr")
+        for i in tr:  # extract download box info
+            key = i.find("th").find_all("span")[1]
+            value = i.find("td")
+            download_box_info += key.text.strip() + ': '
+            if key.text == 'تعداد بازدید':
+                value = value.find('span').find('i').get('data-id')
+                r = requests.get(API+value)
+                value = r.json()['data'][0]['views']
+                # print(r.json()['data'][0]['views'])
+            else:
+                value = value.text
+
+            download_box_info += value.strip() + '\n'
+
+        # print(downloadbox.prettify())
+        googleplay_link = googleplay_link.get("data-link").strip()
+
+        # Bikes Hill 2.6.0 – بازی مسابقه ای “دوچرخه سواری در تپه ها” اندروید + مود
+        en_name = title.split('–')[0].strip()
+        fa_name = ''.join(title.split('–')[1:]).strip()
+
+        cur.execute(INSERT_APP_QUERY, (fa_name, en_name, last_mod,
+                    download_box_info, googleplay_link, link))
+        conn.commit()
+    except requests.exceptions.ConnectionError:
+        print('can not connect to f{link}', file=sys.stderr)
+    except sqlite3.IntegrityError:
+        pass
+    except Exception as e:
+        print(e)
 
 
-googleplay_link = soup.find("a", class_="shadowed-btn gply-link")
-title = googleplay_link.get("title")
+with open('links.txt', 'r') as file:
+    for app in file.readlines():
+        app = app.strip().split(' ')  # app link, date, time seprated by space
+        link = app[0]
+        last_mod = app[1] + ' ' + app[2]
 
-download_box_info = ''
+        crawl_app(link, last_mod)
 
-tr = soup.find_all("tr")
-for i in tr:  # extract download box info
-    key = i.find("th").find_all("span")[1]
-    value = i.find("td")
-    download_box_info += key.text.strip() + ': '
-    if key.text == 'تعداد بازدید':
-        value = value.find('span').find('i').get('data-id')
-        r = requests.get(API+value)
-        value = r.json()['data'][0]['views']
-        # print(r.json()['data'][0]['views'])
-    else:
-        value = value.text
-
-    download_box_info += value.strip() + '\n'
-print(download_box_info)
-
-# print(downloadbox.prettify())
-googleplay_link = googleplay_link.get("data-link").strip()
-
-en_name_found = True
-fa_name_found = False
-
-fa_name = ''
-en_name = ''
-# Bikes Hill 2.6.0 – بازی مسابقه ای “دوچرخه سواری در تپه ها” اندروید + مود
-en_name = title.split('–')[0]
-fa_name = ''.join(title.split('–')[1:])
-
-print(fa_name)
-cur.execute(INSERT_APP_QUERY, (fa_name, en_name, LAST_MOD,
-            download_box_info, googleplay_link, URL))
-conn.commit()
 conn.close()
+exit(0)
--- crawler/crawl_links.py
+++ crawler/crawl_links.py
@@ -1,19 +1,34 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import sys
+
+counter = 0
+limit = 500
+
 
 def get_apps_link(link):
+    global counter
+    global limit
     r = requests.get(link, headers={"Accept": "application/xml"})
-    results = bs(r.content,'lxml-xml')
+    results = bs(r.content, 'lxml-xml')
     results = results.find_all('url')
 
     lst = []
 
-    for j in results :
-        app_link = j.find('loc').contents[0]
-        lst.append(app_link)
-    
-    return lst
+    if counter > limit:
+        print('max limit reached', file=sys.stderr)
+        exit(0)
 
+    for j in results:
+        counter += 1
+        app_link = j.find('loc').text
+        last_mod = j.find('lastmod').text
+        last_mod = last_mod.split(
+            'T')[0] + ' ' + last_mod.split('T')[1].split('+')[0]  # date + ' ' + time
+        # 'app link, date, time' seprated by space
+        lst.append(app_link + ' ' + last_mod)
+
+    return lst
 
 
 farsroid_sitemap_url = "https://www.farsroid.com/sitemap.xml"
@@ -26,8 +41,8 @@ results = results.find_all('sitemap')
 
 
 for i in results:
-    link = i.find('loc').contents[0]
+    link = i.find('loc').text
 
     if "sitemap-pt" in link:
-        apps_link= get_apps_link(link)
-        print(*apps_link , sep='\n')
+        apps_link = get_apps_link(link)
+        print(*apps_link, sep='\n')
