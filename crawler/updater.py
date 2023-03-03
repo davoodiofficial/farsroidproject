c9f4714faadaa4770c78c7ff2a63cbcead524a07
ali afraz <a.afraz.1380@gmail.com>
2023-03-03 14:30:23 +0330

    some bugs fixed.

--- crawler/updater.py
+++ crawler/updater.py
@@ -1,11 +1,18 @@
 import schedule
-import sqlite3
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup as bs4
 import sys
+import psycopg2
+import psycopg2.extras
 
-con = sqlite3.connect("apps.db")
+HOST = "localhost"
+DATABASE = "farsroid"
+USER = "postgres"
+PASSWORD = "pass"
+
+con = psycopg2.connect(host=HOST, database=DATABASE,
+                       user=USER, password=PASSWORD)
 cur = con.cursor()
 
 API = "https://www.farsroid.com/api/posts/?ids="
@@ -19,10 +26,13 @@ def update_app(link, last_mod):
         googleplay_link = soup.find("a", class_="shadowed-btn gply-link")
         title = googleplay_link.get("title")
 
-        download_links = soup.find_all("a", class_="download-btn")
-        for i in download_links:
+        download_link_app = soup.find_all("a", class_="download-btn")
+        downlaod_links = ''
+        for i in download_link_app:
             dl = i.get("href")
-            size = i.find("span", class_="txt").text.split("-")[1].strip()
+            storage = i.find("span", class_="txt").text.split(
+                " - ")[-1].strip()
+            downlaod_links += dl + ' ' + storage + '\n'
 
         download_box_info = ''
 
@@ -45,35 +55,48 @@ def update_app(link, last_mod):
         en_name = title.split('–')[0].strip()
         fa_name = ''.join(title.split('–')[1:]).strip()
 
-        return f"UPDATE apps SET fa_name = '{fa_name}', en_name = '{en_name}', last_mod = '{last_mod}', download_box = '{download_box_info}', googleplay_link = '{googleplay_link}', farsroid_link = '{link}'"
+        return f"UPDATE apps SET fa_name = '{fa_name}', en_name = '{en_name}', last_mod = '{last_mod}', download_box = '{download_box_info}', googleplay_link = '{googleplay_link}', farsroid_link = '{link}', download_links = '{downlaod_links}'"
     except requests.exceptions.ConnectionError:
         print('can not connect to f{link}', file=sys.stderr)
-    except sqlite3.IntegrityError:
+    except psycopg2.IntegrityError:
         pass
     except Exception as e:
         print(e)
 
 
 def updater():
+    global con
+    global cur
+
     updated_apps = str()
-    res = cur.execute("SELECT * FROM apps")
-    res = res.fetchall()
+    cur.execute("SELECT * FROM apps")
+    res = cur.fetchall()
     for i in res:
         old_date = datetime.strptime(
             i[3], '%Y-%m-%d %H:%M:%S')  # i3 is last mod
 
-        r = requests.get(i[6])  # i6 is farsroid link
-        soup = bs4(r.content, 'html.parser')
-        data_tiemstamp = soup.find('ul', class_='post-infs').find_all(
-            'li')[1].find('div').find('div').get('data-timestamp')
-        new_date = datetime.strptime(
-            data_tiemstamp, '%Y-%m-%d %H:%M:%S')
+        try:
+            r = requests.get(i[6])  # i6 is farsroid link
+            soup = bs4(r.content, 'html.parser')
+            data_tiemstamp = soup.find('ul', class_='post-infs').find_all(
+                'li')[1].find('div').find('div').get('data-timestamp')
+            new_date = datetime.strptime(
+                data_tiemstamp, '%Y-%m-%d %H:%M:%S')
 
-        if old_date < new_date:
-            update_query = update_app(
-                i[6], data_tiemstamp) + f" WHERE id = {i[0]};"
-            updated_apps += update_query
-            print(update_query)
+            if old_date < new_date:
+                update_query = update_app(
+                    i[6], data_tiemstamp) + f" WHERE id = {i[0]};"
+                updated_apps += update_query
+        except requests.exceptions.ConnectionError:
+            print('can not connect to f{link}', file=sys.stderr)
+        except AttributeError:
+            print('can not get last mod:', i[6], file=sys.stderr)
+        except Exception as e:
+            print(e)
+        else:
+            print('is up to date:', i[6])
+
+    print(updated_apps)
 
 
 updater()
