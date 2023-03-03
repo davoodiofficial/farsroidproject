dd80d591afb0cd261dd24152efe40b53368e7f5f
ali afraz <a.afraz.1380@gmail.com>
2023-03-03 04:41:30 +0330

    database changed to postgresql. fix some bugs in updater and crawl app.

--- crawler/crawl_apps.py
+++ crawler/crawl_apps.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-import sqlite3
+import psycopg2
 import sys
 
-DB_NAME = "apps.db"
+HOST = "localhost"
+DATABASE = "farsroid"
+USER = "postgres"
+PASSWORD = "pass"
 API = "https://www.farsroid.com/api/posts/?ids="
 CREATE_APP_TABLE_QUERY = """CREATE TABLE IF NOT EXISTS apps (
                                     id integer PRIMARY KEY,
@@ -12,14 +15,12 @@ CREATE_APP_TABLE_QUERY = """CREATE TABLE IF NOT EXISTS apps (
                                     last_mod text NOT NULL,
                                     download_box text NOT NULL,
                                     googleplay_link text NOT NULL UNIQUE,
-                                    farsroid_link text NOT NULL UNIQUE
+                                    farsroid_link text NOT NULL UNIQUE,
+                                    download_links text NOT NULL
                                 );"""
-INSERT_APP_QUERY = """INSERT INTO apps(fa_name,en_name,last_mod,download_box,googleplay_link,farsroid_link)
-              VALUES(?,?,?,?,?,?)"""
-
-conn = sqlite3.connect(DB_NAME)
-cur = conn.cursor()
-cur.execute(CREATE_APP_TABLE_QUERY)
+INSERT_APP_QUERY = """INSERT INTO apps(
+	fa_name, en_name, last_mod, download_box, googleplay_link, farsroid_link, download_links)
+	VALUES (%s, %s, %s, %s, %s, %s, %s);"""
 
 
 def crawl_app(link, last_mod):
@@ -29,24 +30,19 @@ def crawl_app(link, last_mod):
     try:
         page = requests.get(link)
         soup = BeautifulSoup(page.content, "html.parser")
-        downloadbox = soup.find("table", class_="post-metas-tabeld")
-        infos = downloadbox.find("tbody")
 
         googleplay_link = soup.find("a", class_="shadowed-btn gply-link")
         title = googleplay_link.get("title")
 
-        download_link_app =soup.find_all("a", class_="download-btn")
+        download_link_app = soup.find_all("a", class_="download-btn")
+        downlaod_links = ''
         for i in download_link_app:
             dl = i.get("href")
-            print(dl)
-            stroge = i.find("span",class_="txt").text.split("-")[1].strip()
-            print(stroge)
-
-        
-
+            storage = i.find("span", class_="txt").text.split(
+                " - ")[-1].strip()
+            downlaod_links += dl + ' ' + storage + '\n'
 
         download_box_info = ''
-
         tr = soup.find_all("tr")
         for i in tr:  # extract download box info
             key = i.find("th").find_all("span")[1]
@@ -56,39 +52,42 @@ def crawl_app(link, last_mod):
                 value = value.find('span').find('i').get('data-id')
                 r = requests.get(API+value)
                 value = r.json()['data'][0]['views']
-                # print(r.json()['data'][0]['views'])
             else:
                 value = value.text
 
             download_box_info += value.strip() + '\n'
 
-        # print(downloadbox.prettify())
         googleplay_link = googleplay_link.get("data-link").strip()
 
         # Bikes Hill 2.6.0 – بازی مسابقه ای “دوچرخه سواری در تپه ها” اندروید + مود
         en_name = title.split('–')[0].strip()
         fa_name = ''.join(title.split('–')[1:]).strip()
-        
+
         cur.execute(INSERT_APP_QUERY, (fa_name, en_name, last_mod,
-                    download_box_info, googleplay_link, link))
+                                       download_box_info, googleplay_link, link, downlaod_links))
         conn.commit()
     except requests.exceptions.ConnectionError:
         print('can not connect to f{link}', file=sys.stderr)
-    except sqlite3.IntegrityError:
+    except psycopg2.IntegrityError:
         pass
     except Exception as e:
         print(e)
+        conn.rollback()
+    else:
+        print('inserted:', link, flush=True)
 
 
-with open('links.txt', 'r') as file:
-    for app in file.readlines():
-        app = app.strip().split(' ')  # app link, date, time seprated by space
-        link = app[0]
-        last_mod = app[1] + ' ' + app[2]
-
-        crawl_app(link, last_mod)
-
-conn.close()
-exit(0)
-
-
+if __name__ == "__main__":
+    conn = psycopg2.connect(host=HOST, database=DATABASE,
+                            user=USER, password=PASSWORD)
+    cur = conn.cursor()
+    print("PostgreSQL server information")
+    print(conn.get_dsn_parameters(), "\n")
+    with open('links.txt', 'r') as file:
+        for app in file.readlines():
+            app = app.strip().split(' ')  # app link, date, time seprated by space
+            link = app[0]
+            last_mod = app[1] + ' ' + app[2]
+            crawl_app(link, last_mod)
+    conn.close()
+    exit(0)
--- crawler/updater.py
+++ crawler/updater.py
@@ -1,18 +1,79 @@
 import schedule
 import sqlite3
 from datetime import datetime
-import crawl_apps
+import requests
+from bs4 import BeautifulSoup as bs4
+import sys
 
 con = sqlite3.connect("apps.db")
 cur = con.cursor()
 
+API = "https://www.farsroid.com/api/posts/?ids="
+
+
+def update_app(link, last_mod):
+    try:
+        page = requests.get(link)
+        soup = bs4(page.content, "html.parser")
+
+        googleplay_link = soup.find("a", class_="shadowed-btn gply-link")
+        title = googleplay_link.get("title")
+
+        download_links = soup.find_all("a", class_="download-btn")
+        for i in download_links:
+            dl = i.get("href")
+            size = i.find("span", class_="txt").text.split("-")[1].strip()
+
+        download_box_info = ''
+
+        tr = soup.find_all("tr")
+        for i in tr:  # extract download box info
+            key = i.find("th").find_all("span")[1]
+            value = i.find("td")
+            download_box_info += key.text.strip() + ': '
+            if key.text == 'تعداد بازدید':
+                value = value.find('span').find('i').get('data-id')
+                r = requests.get(API+value)
+                value = r.json()['data'][0]['views']
+            else:
+                value = value.text
+
+            download_box_info += value.strip() + '\n'
+
+        googleplay_link = googleplay_link.get("data-link").strip()
+
+        en_name = title.split('–')[0].strip()
+        fa_name = ''.join(title.split('–')[1:]).strip()
+
+        return f"UPDATE apps SET fa_name = '{fa_name}', en_name = '{en_name}', last_mod = '{last_mod}', download_box = '{download_box_info}', googleplay_link = '{googleplay_link}', farsroid_link = '{link}'"
+    except requests.exceptions.ConnectionError:
+        print('can not connect to f{link}', file=sys.stderr)
+    except sqlite3.IntegrityError:
+        pass
+    except Exception as e:
+        print(e)
+
 
 def updater():
+    updated_apps = str()
     res = cur.execute("SELECT * FROM apps")
     res = res.fetchall()
     for i in res:
-        old_date = datetime.strptime(i[3], '%Y-%m-%d %H:%M:%S')
+        old_date = datetime.strptime(
+            i[3], '%Y-%m-%d %H:%M:%S')  # i3 is last mod
+
+        r = requests.get(i[6])  # i6 is farsroid link
+        soup = bs4(r.content, 'html.parser')
+        data_tiemstamp = soup.find('ul', class_='post-infs').find_all(
+            'li')[1].find('div').find('div').get('data-timestamp')
+        new_date = datetime.strptime(
+            data_tiemstamp, '%Y-%m-%d %H:%M:%S')
+
         if old_date < new_date:
-            crawl_apps.crawl_app(i[6], new_date)
+            update_query = update_app(
+                i[6], data_tiemstamp) + f" WHERE id = {i[0]};"
+            updated_apps += update_query
+            print(update_query)
+
 
 updater()
